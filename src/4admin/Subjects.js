8d791f51fd05cd4462d9fa76bf8c12dd1abbad84
wilson11 <wilson@gmail.com>
2023-03-11 20:56:18 +0800

    sample 5

--- src/4admin/Subjects.js
+++ src/4admin/Subjects.js
@@ -1,56 +1,66 @@
-const [subject, setsubjects] = useState();
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
-useEffect(()=>{
-  axios.get('https:api.kyusilid.online/getallsubjects/1').then(result=>{
-    setsubjects(result.data)
-  }).catch()
-},[])
 
-return (
-  <div className='tertiary borderradius-lg padding12'>
-   
-   <div className="flex"> 
-    <div>
-    <h4>Subjects table</h4>
-    <p className='smallfont'> last updated :</p>
-    </div>
 
-    <input type="text" placeholder='Search' className='marginleftauto' />
-    </div>
+function Subjects() {
+ 
+  const [subject, setsubjects] = useState();
 
-    <div className="persontable margintop12 width100">
-    <table className='width100' cellSpacing={0}>
-     <thead className='primary'>
-      <th>Year</th>
-      <th>Subject code</th>
-      <th>Subject name</th>
-      <th>Classes</th>
-      <th></th>
+  useEffect(()=>{
+    axios.get('https:api.kyusilid.online/getallsubjects/1').then(result=>{
+      setsubjects(result.data)
+    }).catch()
+  },[])
+
+  return (
+    <div className='tertiary borderradius-lg padding12'>
+     
+     <div className="flex"> 
+      <div>
+      <h4>Subjects table</h4>
+      <p className='smallfont'> last updated :</p>
+      </div>
+
+      <input type="text" placeholder='Search' className='marginleftauto' />
+      </div>
+
+      <div className="persontable margintop12 width100">
+      <table className='width100' cellSpacing={0}>
+       <thead className='primary'>
+        <th>Year</th>
+        <th>Subject code</th>
+        <th>Subject name</th>
+        <th>Classes</th>
+        <th></th>
+        
+       </thead>
+       <tbody>
+
+        {/* {subject !== undefined  && subject.map((item, key)=>(
+            <tr key= {key}>
+
+        
+            <td>{item.units}</td>
+            <td>{item.sub_code}</td>
+            <td>{item.sub_name}</td>
+            <td></td>
+            <td>
+             </td>
+          </tr>
+
+        ))} */}
       
-     </thead>
-     <tbody>
-
-      {subject !== undefined  && subject.map((item, key)=>(
-          <tr key= {key}>
-
-      
-          <td>{item.units}</td>
-          <td>{item.sub_code}</td>
-          <td>{item.sub_name}</td>
-          <td></td>
-          <td>
-           </td>
-        </tr>
-
-      ))}
+        
+       </tbody>
+     
+      </table>
+    </div>
     
       
-     </tbody>
-   
-    </table>
-  </div>
-  
     
-  
-    </div>
-)
+      </div>
+  )
+}
+
+export default Subjects
