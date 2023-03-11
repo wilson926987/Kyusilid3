baacc86186a4de24d11b1a2018b470f19ef04db9
wilson11 <wilson@gmail.com>
2023-03-11 20:52:17 +0800

    sample4

--- src/4admin/Subjects.js
+++ src/4admin/Subjects.js
@@ -1,11 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+const [subject, setsubjects] = useState();
 
+useEffect(()=>{
+  axios.get('https:api.kyusilid.online/getallsubjects/1').then(result=>{
+    setsubjects(result.data)
+  }).catch()
+},[])
 
+return (
+  <div className='tertiary borderradius-lg padding12'>
+   
+   <div className="flex"> 
+    <div>
+    <h4>Subjects table</h4>
+    <p className='smallfont'> last updated :</p>
+    </div>
 
-function Subjects() {
- 
- <div></div>
-}
+    <input type="text" placeholder='Search' className='marginleftauto' />
+    </div>
 
-export default Subjects
+    <div className="persontable margintop12 width100">
+    <table className='width100' cellSpacing={0}>
+     <thead className='primary'>
+      <th>Year</th>
+      <th>Subject code</th>
+      <th>Subject name</th>
+      <th>Classes</th>
+      <th></th>
+      
+     </thead>
+     <tbody>
+
+      {subject !== undefined  && subject.map((item, key)=>(
+          <tr key= {key}>
+
+      
+          <td>{item.units}</td>
+          <td>{item.sub_code}</td>
+          <td>{item.sub_name}</td>
+          <td></td>
+          <td>
+           </td>
+        </tr>
+
+      ))}
+    
+      
+     </tbody>
+   
+    </table>
+  </div>
+  
+    
+  
+    </div>
+)
