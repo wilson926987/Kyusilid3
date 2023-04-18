62ab41e980a9b99fa2718eda1dcf3bc30d32aa59
jcuber06016 <jcuber0616@gmail.com>
2023-04-18 13:17:31 +0800

    attendance

--- src/1general/containerpages/Attendance.js
+++ src/1general/containerpages/Attendance.js
@@ -1,65 +1,91 @@
-import { getValue } from '@testing-library/user-event/dist/utils'
-import React, { useContext, useEffect, useState } from 'react'
-import { userInfoContext } from '../../Globalcontext'
-import Avatar from '../../assets/images/avatar.jpg'
-import { personlistContext } from '../../Globalcontext'
+import React, { useContext, useEffect, useState } from 'react';
+import { userInfoContext } from '../../Globalcontext';
+import Avatar from '../../assets/images/avatar.jpg';
+import { personlistContext , currentclassContext } from '../../Globalcontext';
+import axios from 'axios';
 
+function Attendance({ classId }) {
+  const { userinfo } = useContext(userInfoContext);
+  const { personlist } = useContext(personlistContext);
+  const [attendanceList, setAttendanceList] = useState([]);
+  const [search, setSearch] = useState('');
+  const [attendances, setAttendances] = useState([]);
+  const {currentclass} = useContext(currentclassContext)
+  
+  useEffect(() => {
+    console.log(currentclass)
 
-function Attendance() {
+    if (currentclass !== undefined) {
+      axios.get(`https://api.kyusillid.online/api/getAttendance/${currentclass.classes_id}`)
+        .then(response => {
+          setAttendances(response.data.attendances);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }, [currentclass]);
 
-
-  const {userinfo} = useContext(userInfoContext)
-  const {personlist} = useContext(personlistContext)
-
-  useEffect(()=>{
-    console.log(personlist)
-  },[])
 
   return (
-    <div>
-      <div className='flex'> <h4>Attendance</h4> </div>
-
-  <div class="Searrchh">     
-      <div class="search1">
-        <input
-          type="text"
-          placeholder="Search by name"
-          name="search"
-        />
-        </div>
-
-</div>
-
- 
-        <div className="persontable">
-
-
-<table className="attendancetable">  
-
-  <tr className="stickheader primary vertical">
-            <th>Name</th>
-            
-  </tr>
-
-  {personlist !== null &&
-  personlist.filter(e=>e.usertype  ==="stud").map((item , key) =>(
-    <tr key={key}>
-  <td className='tdname flex' > <img src={Avatar} alt="" /> {item.firstname} {item.middle} {item.lastname} {item.suffix}</td>
-</tr>
-  ))
-  
-  }
-
-
-
-</table>
-        </div>
-    
-      </div>
-    
-    
-
-  )
+        <div>
+      <h2>Attendance</h2>
+      <ul>
+        {attendances.map(attendance => (
+          <li key={attendance.id}>
+            {attendance.lastname + ", " + attendance.firstname} - {attendance.att_status} - {attendance.date_created}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Attendance
+export default Attendance;
+//     <div>
+//       <div className="flex">
+//         <h4>Attendance</h4>
+//       </div>
+
+//       <div className="Searrchh">
+//         <div className="search1">
+//           <input
+//             type="text"
+//             placeholder="Search by name"
+//             value={search}
+//             onChange={(e) => setSearch(e.target.value.toLowerCase())}
+//           />
+//         </div>
+//       </div>
+
+//       <div className="persontable">
+//         <table className="attendancetable col-lg-12">
+//           <thead>
+//             <tr className="stickheader primary vertical">
+//               <th>Name</th>
+//               <th>Status</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {personlist !== null &&
+//               personlist
+//                 .filter((person) => person.usertype === 'stud')
+//                 .filter((person) => person.lastname.toLowerCase().includes(search))
+//                 .map((person) => (
+//                   <tr key={person.id}>
+//                     <td className="tdname flex">
+//                       <img src={Avatar} alt="" />
+//                       {person.lastname}, {person.firstname} {person.middle} {person.suffix}
+//                     </td>
+//                     <td>{getAttendanceStatus(person.id)}</td>
+//                   </tr>
+//                 ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Attendance;
