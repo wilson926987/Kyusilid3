6dfbeb1f8b38a783a524d606518f37496c56a87a
wilson11 <wilson@gmail.com>
2023-05-05 08:59:50 +0800

    adminpost update

--- src/1general/components/Adminpostpanel.js
+++ src/1general/components/Adminpostpanel.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import {HiSpeakerphone} from 'react-icons/hi'
 import Avatar from '../../assets/images/avatar.jpg'
+import Swal from 'sweetalert2'
+
+
 
 function Adminpostpanel({item}) {
+
+  const fire= (e)=>{
+     
+    Swal.fire({
+    
+   
+      text: e
+    
+    })
+  }
   return (
-    <div className='adminpostpanel borderradius-md'>
+    <div className='adminpostpanel borderradius-md' onClick={()=>{fire(item.announcement_content)}}>
       <div className='adminposticons'>
         <div>
             <HiSpeakerphone />
